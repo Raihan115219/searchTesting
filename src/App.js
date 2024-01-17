65f78ac25efabc5d68a22dd32bcc4b1d93be4074
Raihan115219 <aburaihan115219@gmail.com>
2024-01-18 00:48:53 +0600

    search functinality and api call testing prcatice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import User from "./User";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = ({ target: { value } }) => {
+    setSearchTerm(value);
+  };
+
+  const fetchUsers = async () => {
+    const res = await fetch("https://reqres.in/api/users/", { method: "GET" });
+    const json = await res.json();
+    setUsers(json.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+      <input
+        className="input-box"
+        type="text"
+        placeholder="Search users"
+        value={searchTerm}
+        onChange={(e) => handleChange(e)}
+      />
+      <User users={users} searchTerm={searchTerm} />
     </div>
   );
-}
+};
 
 export default App;
